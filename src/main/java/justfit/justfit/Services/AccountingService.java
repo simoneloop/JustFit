e0ff1f6e6aee6affc846e99a76e574146f24a44a
simoneloop <simonelopez@hotmail.it>
2021-04-22 02:16:39 +0200

    accService

---
+++ src/main/java/justfit/justfit/Services/AccountingService.java
@@ -0,0 +1,28 @@
+package justfit.justfit.Services;
+
+import justfit.justfit.entities.Users;
+import justfit.justfit.repositories.UserRepository;
+import justfit.justfit.utils.exception.EmailAlreadyExistException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class AccountingService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Transactional
+    public Users registerUser(Users users) throws Exception {
+        /*
+            TODO controlli su i dati inseriti
+         */
+        if(userRepository.existsByEmail(users.getEmail())){
+            throw new EmailAlreadyExistException();
+        }
+        return userRepository.save(users);
+    }
+}
